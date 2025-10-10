34b0cb206c4834fbaa94bf0195a266c955789836
Reyaz Ahamd Bhat <reyazbhat1881@gmail.com>
2025-10-10 22:05:57 +0530

    added a weather api

---
+++ weather-app.js
@@ -0,0 +1,15 @@
+// http://api.weatherapi.com/v1/current.json?key=0c01f5a6c9644114842155832251010&q=srinagar&aqi=no
+
+let targetLocation = 'jammu'
+
+const fetchDetails = async(targetLocation) =>{
+let url = `http://api.weatherapi.com/v1/current.json?key=0c01f5a6c9644114842155832251010&q=${targetLocation}&aqi=no
+`
+const res = await fetch(url)
+
+const data = await res.json()
+
+console.log(data);
+
+}
+fetchDetails()
